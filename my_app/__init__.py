a53070cef7bc2c509455dadbd361afa40da1dee0
shemar100 <anderson.shemar17@gmail.com>
2021-09-07 23:25:52 -0500

    removed csrf config

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -1,19 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_wtf.csrf import CSRFProtect
 from flask_restful import Api
 
-
-ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
-
-# csrf = CSRFProtect(app)
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
